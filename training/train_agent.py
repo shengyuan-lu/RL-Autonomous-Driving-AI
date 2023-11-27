300d8016bbcd45f12818bef1ca38d413fff847fd
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-11-27 11:09:39 -0800

    set training parameter for model 8

--- training/train_agent.py
+++ training/train_agent.py
@@ -149,10 +149,10 @@ if __name__ == '__main__':
     }
 
     trainer = ModelTrainer(new_model_name='PPO_highway',
-                           exist_model_name='best_model',
+                           exist_model_name='PPO_highway_7',
                            train_new_model_hyperparam={},
                            train_exist_model_hyperparam=hyperparams,
                            total_timesteps=100000)
 
-    trainer.train_model(train_new=True)
+    trainer.train_model(train_new=False)
 
